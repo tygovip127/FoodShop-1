9370c4152a0b6349edf458adc76d035ba6b6ad60
Your Name <you@example.com>
2021-10-15 00:02:48 +0700

    header, footer

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <link
+    href="{{ asset('https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css')}}"
+    rel="stylesheet">
+  <link href="{{ asset('https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css') }}" rel="stylesheet">
+
+  <link rel="stylesheet" href="{{ asset('../../css/vendor/vendor.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('../../plugins/plugins.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('../../css/style.min.css')}}">
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="main-wrap">
+    @include('layouts.header')
+    @yield('content')
+
+    <p>Hello every body</p>
+    @include('layouts.footer')
+  </div>
+</body>
+
+</html>
