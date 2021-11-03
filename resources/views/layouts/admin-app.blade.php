77f27aedc52e542e53620962b15127a66d4783bc
Le Phu Quoc <tygovip119@gmail.com>
2021-11-03 15:04:25 +0700

    Update interface admin: sidebar (Uncompleted)

--- resources/views/layouts/admin-app.blade.php
+++ resources/views/layouts/admin-app.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  
+
   <link rel="stylesheet" href="{{ asset('../../css/vendor/bootstrap.min.css')}}">
   <link rel="stylesheet" href="{{ asset('../../css/vendor/signericafat.css')}}">
   <link rel="stylesheet" href="{{ asset('../../css/vendor/cerebrisans.css')}}">
@@ -24,16 +24,32 @@
   <link rel="stylesheet" href="{{ asset('../../plugins/plugins.min.css')}}">
   <link rel="stylesheet" href="{{ asset('../../css/style.min.css')}}">
   <link rel="stylesheet" href="{{ asset('../../css/all.min.css')}}">
-  <link rel="stylesheet" href="{{ asset('../../css/plugins/soft-ui-dashboard.min.css')}}">
+
+  <!-- Dashboard chart CSS -->
+  <link rel="stylesheet" href="{{ asset('../../css/soft-ui-dashboard.min.css')}}">
+
+  <!-- General CSS Files Sidebar -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  <!-- CSS Libraries Sidebar -->
+  <link rel="stylesheet" href="../node_modules/jqvmap/dist/jqvmap.min.css">
+  <link rel="stylesheet" href="../node_modules/summernote/dist/summernote-bs4.css">
+  <link rel="stylesheet" href="../node_modules/owl.carousel/dist/assets/owl.carousel.min.css">
+  <link rel="stylesheet" href="../node_modules/owl.carousel/dist/assets/owl.theme.default.min.css">
+
+  <!-- Template CSS Sidebar -->
+  <link rel="stylesheet" href="{{ asset('../../css/style-sidebar.css') }}">
+  <link rel="stylesheet" href="{{ asset('../../css/components-sidebar.css') }}">
   <title>@yield('title')</title>
 </head>
 
 <body>
   <div class="main-wrapper">
     @include('layouts.admin-header')
-
+    @include('layouts.admin-sidebar')
     @yield('content')
-    
+
 
     @include('layouts.admin-footer')
   </div>
@@ -55,11 +71,32 @@
   <script src="{{ asset('../../js/plugins/ajax-mail.js') }}"></script>
 
   <script src="{{ asset('../../js/main.js') }}"></script>
-   <!--   Core JS Files   -->
-   <script src="{{ asset('../../js/plugins/chartjs.min.js') }}"></script>
+  <!--   Core JS Files Dashboard -->
+  <script src="{{ asset('../../js/plugins/chartjs.min.js') }}"></script>
   <script src="{{ asset('../../js/plugins/Chart.extension.js') }}"></script>
   <script src="{{ asset('../../js/plugins/chart-view.js') }}"></script>
-</body>
+
+  <!-- General JS Scripts Sidebar -->
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+  <script src="{{ asset('../../js/stisla-sidebar.js') }}"></script>
+
+  <!-- JS Libraies Sidebar -->
+  <script src="../node_modules/jquery-sparkline/jquery.sparkline.min.js"></script>
+  <script src="../node_modules/chart.js/dist/Chart.min.js"></script>
+  <script src="../node_modules/owl.carousel/dist/owl.carousel.min.js"></script>
+  <script src="../node_modules/summernote/dist/summernote-bs4.js"></script>
+  <script src="../node_modules/chocolat/dist/js/jquery.chocolat.min.js"></script>
+
+  <!-- Template JS File Sidebar -->
+  <script src="{{ asset('../../js/scripts-sidebar.js') }}"></script>
+  <script src="{{ asset('../../js/custom-sidebar.js') }}"></script>
+
+  <!-- Page Specific JS File Sidebar-->
+  <script src="{{ asset('../../js/index-sidebar.js') }}"></script>
 </body>
 
 </html>
--- resources/views/layouts/admin-header.blade.php
+++ resources/views/layouts/admin-header.blade.php
@@ -1,4 +1,10 @@
-<header class="header-area">
+<nav class="navbar navbar-expand-lg main-navbar">
+  <form>
+    <ul class="navbar-nav mr-3">
+      <li><a href="" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+    </ul>
+  </form>
+
   <div class="container">
     <div class="header-large-device">
       <div class="header-bottom">
@@ -6,74 +12,6 @@
           <div class="col-xl-2 col-lg-2">
             <div class="logo">
               <a href="/"><img src="{{ asset('images/logo/logo.png') }}" alt="logo"></a>
-            </div>
-          </div>
-          <div class="col-xl-8 col-lg-7">
-            <div class="main-menu main-menu-padding-1 main-menu-lh-1">
-              <nav>
-                <ul>
-                  <li>
-                    <a href="/">HOME </a>
-                  </li>
-                  <li><a href="/products">SHOP </a>
-                    <ul class="mega-menu-style mega-menu-mrg-1">
-                      <li>
-                        <ul>
-                          <li>
-                            <a class="dropdown-title" href="#">Shop Layout</a>
-                            <ul>
-                              <li><a href="shop.html">standard style</a></li>
-                              <li><a href="shop-list.html">shop list style</a></li>
-                              <li><a href="shop-fullwide.html">shop fullwide</a></li>
-                              <li><a href="shop-no-sidebar.html">grid no sidebar</a></li>
-                              <li><a href="shop-list-no-sidebar.html">list no sidebar</a></li>
-                              <li><a href="shop-right-sidebar.html">shop right sidebar</a></li>
-                              <li><a href="store-location.html">store location</a></li>
-                            </ul>
-                          </li>
-                          <li>
-                            <a class="dropdown-title" href="#">Products Layout</a>
-                            <ul>
-                              <li><a href="product-details.html">tab style 1</a></li>
-                              <li><a href="product-details-2.html">tab style 2</a></li>
-                              <li><a href="product-details-sticky.html">sticky style</a></li>
-                              <li><a href="product-details-gallery.html">gallery style </a></li>
-                              <li><a href="product-details-affiliate.html">affiliate style</a></li>
-                              <li><a href="product-details-group.html">group style</a></li>
-                              <li><a href="product-details-fixed-img.html">fixed image style </a></li>
-                            </ul>
-                          </li>
-                          <li>
-                            <a href="shop.html"><img src="assets/images/banner/banner-12.png" alt=""></a>
-                          </li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </li>
-                  <li><a href="#">PAGES </a>
-                    <ul class="sub-menu-style">
-                      <li><a href="about-us.html">about us </a></li>
-                      <li><a href="cart.html">cart page</a></li>
-                      <li><a href="checkout.html">checkout </a></li>
-                      <li><a href="my-account.html">my account</a></li>
-                      <li><a href="/wishlist">wishlist </a></li>
-                      <li><a href="compare.html">compare </a></li>
-                      <li><a href="contact.html">contact us </a></li>
-                      <li><a href="order-tracking.html">order tracking</a></li>
-                      <li><a href="/login-register">login / register </a></li>
-                    </ul>
-                  </li>
-                  <li><a href="blog.html">BLOG </a>
-                    <ul class="sub-menu-style">
-                      <li><a href="blog.html">blog standard </a></li>
-                      <li><a href="blog-no-sidebar.html">blog no sidebar </a></li>
-                      <li><a href="blog-right-sidebar.html">blog right sidebar</a></li>
-                      <li><a href="blog-details.html">blog details</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="contact.html">CONTACT </a></li>
-                </ul>
-              </nav>
             </div>
           </div>
           <div class="col-xl-2 col-lg-3">
@@ -90,13 +28,13 @@
                   </form>
                 </div>
               </div>
-              <div class="same-style-2 main-menu" >
+              <div class="same-style-2 main-menu">
                 <nav>
                   <ul>
                     <li>
                       <a href="/login-register"><i class="icon-user"></i></a>
                       <ul class="sub-menu-style">
-                        <li ><a href="/account">My Account </a></li>
+                        <li><a href="/account">My Account </a></li>
                         <li><a href="/logout">Logout </a></li>
                         <li><a href="/dashboard">Admin </a></li>
                       </ul>
@@ -118,175 +56,34 @@
         </div>
       </div>
     </div>
-    <div class="header-small-device small-device-ptb-1">
-      <div class="row align-items-center">
-        <div class="col-5">
-          <div class="mobile-logo">
-            <a href="index.html">
-              <img alt="" src="{{ asset('images/logo/logo.png') }}">
+  </div>
+  <div class="header-small-device small-device-ptb-1">
+    <div class="row align-items-center">
+      <div class="col-5">
+        <div class="mobile-logo">
+          <a href="index.html">
+            <img alt="" src="{{ asset('images/logo/logo.png') }}">
+          </a>
+        </div>
+      </div>
+      <div class="col-7">
+        <div class="header-action header-action-flex">
+          <div class="same-style-2">
+            <a href="login-register.html"><i class="icon-user"></i></a>
+          </div>
+          <div class="same-style-2">
+            <a href="/wishlist"><i class="icon-heart"></i><span class="pro-count red">03</span></a>
+          </div>
+          <div class="same-style-2 header-cart">
+            <a class="cart-active" href="#">
+              <i class="icon-basket-loaded"></i><span class="pro-count red">02</span>
             </a>
           </div>
-        </div>
-        <div class="col-7">
-          <div class="header-action header-action-flex">
-            <div class="same-style-2">
-              <a href="login-register.html"><i class="icon-user"></i></a>
-            </div>
-            <div class="same-style-2">
-              <a href="/wishlist"><i class="icon-heart"></i><span class="pro-count red">03</span></a>
-            </div>
-            <div class="same-style-2 header-cart">
-              <a class="cart-active" href="#">
-                <i class="icon-basket-loaded"></i><span class="pro-count red">02</span>
-              </a>
-            </div>
-            <div class="same-style-2 main-menu-icon">
-              <a class="mobile-header-button-active" href="#"><i class="icon-menu"></i> </a>
-            </div>
+          <div class="same-style-2 main-menu-icon">
+            <a class="mobile-header-button-active" href="#"><i class="icon-menu"></i> </a>
           </div>
         </div>
       </div>
     </div>
   </div>
-</header>
-<!-- mini cart start -->
-<div class="sidebar-cart-active">
-  <div class="sidebar-cart-all">
-    <a class="cart-close" href="#"><i class="icon_close"></i></a>
-    <div class="cart-content">
-      <h3>Shopping Cart</h3>
-      <ul>
-        <li class="single-product-cart">
-          <div class="cart-img">
-            <a href="#"><img src="assets/images/cart/cart-1.jpg" alt=""></a>
-          </div>
-          <div class="cart-title">
-            <h4><a href="#">Simple Black T-Shirt</a></h4>
-            <span> 1 × $49.00 </span>
-          </div>
-          <div class="cart-delete">
-            <a href="#">×</a>
-          </div>
-        </li>
-        <li class="single-product-cart">
-          <div class="cart-img">
-            <a href="#"><img src="assets/images/cart/cart-2.jpg" alt=""></a>
-          </div>
-          <div class="cart-title">
-            <h4><a href="#">Norda Backpack</a></h4>
-            <span> 1 × $49.00 </span>
-          </div>
-          <div class="cart-delete">
-            <a href="#">×</a>
-          </div>
-        </li>
-      </ul>
-      <div class="cart-total">
-        <h4>Subtotal: <span>$170.00</span></h4>
-      </div>
-      <div class="cart-checkout-btn">
-        <a class="btn-hover cart-btn-style" href="/cart">view cart</a>
-        <a class="no-mrg btn-hover cart-btn-style" href="checkout.html">checkout</a>
-      </div>
-    </div>
-  </div>
-</div>
-
-<!-- Mobile menu start -->
-<div class="mobile-header-active mobile-header-wrapper-style">
-  <div class="clickalbe-sidebar-wrap">
-    <a class="sidebar-close"><i class="icon_close"></i></a>
-    <div class="mobile-header-content-area">
-      <div class="mobile-search mobile-header-padding-border-1">
-        <form class="search-form" action="#">
-          <input type="text" placeholder="Search here…">
-          <button class="button-search"><i class="icon-magnifier"></i></button>
-        </form>
-      </div>
-      <div class="mobile-menu-wrap mobile-header-padding-border-2">
-        <!-- mobile menu start -->
-        <nav>
-          <ul class="mobile-menu">
-            <li class="menu-item-has-children"><span class="menu-expand"><i></i></span><a href="index.html">Home</a>
-              <ul class="dropdown" style="display: none;">
-                <li><a href="index.html">Home version 1 </a></li>
-                <li><a href="index-2.html">Home version 2</a></li>
-                <li><a href="index-3.html">Home version 3</a></li>
-                <li><a href="index-4.html">Home version 4</a></li>
-                <li><a href="index-5.html">Home version 5</a></li>
-                <li><a href="index-6.html">Home version 6</a></li>
-                <li><a href="index-7.html">Home version 7</a></li>
-                <li><a href="index-8.html">Home version 8</a></li>
-                <li><a href="index-9.html">Home version 9</a></li>
-                <li><a href="index-10.html">Home version 10</a></li>
-              </ul>
-            </li>
-            <li class="menu-item-has-children "><span class="menu-expand"><i></i></span><a href="#">shop</a>
-              <ul class="dropdown" style="display: none;">
-                <li class="menu-item-has-children"><span class="menu-expand"><i></i></span><a href="#">shop layout</a>
-                  <ul class="dropdown" style="display: none;">
-                    <li><a href="shop.html">standard style</a></li>
-                    <li><a href="shop-list.html">shop list style</a></li>
-                    <li><a href="shop-fullwide.html">shop fullwide</a></li>
-                    <li><a href="shop-no-sidebar.html">grid no sidebar</a></li>
-                    <li><a href="shop-list-no-sidebar.html">list no sidebar</a></li>
-                    <li><a href="shop-right-sidebar.html">shop right sidebar</a></li>
-                    <li><a href="store-location.html">store location</a></li>
-                  </ul>
-                </li>
-                <li class="menu-item-has-children"><span class="menu-expand"><i></i></span><a href="#">Products
-                    Layout</a>
-                  <ul class="dropdown" style="display: none;">
-                    <li><a href="product-details.html">tab style 1</a></li>
-                    <li><a href="product-details-2.html">tab style 2</a></li>
-                    <li><a href="product-details-sticky.html">sticky style</a></li>
-                    <li><a href="product-details-gallery.html">gallery style </a></li>
-                    <li><a href="product-details-affiliate.html">affiliate style</a></li>
-                    <li><a href="product-details-group.html">group style</a></li>
-                    <li><a href="product-details-fixed-img.html">fixed image style </a></li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li class="menu-item-has-children"><span class="menu-expand"><i></i></span><a href="#">Pages</a>
-              <ul class="dropdown" style="display: none;">
-                <li><a href="about-us.html">about us </a></li>
-                <li><a href="cart.html">cart page</a></li>
-                <li><a href="checkout.html">checkout </a></li>
-                <li><a href="my-account.html">my account</a></li>
-                <li><a href="wishlist.html">wishlist </a></li>
-                <li><a href="compare.html">compare </a></li>
-                <li><a href="contact.html">contact us </a></li>
-                <li><a href="order-tracking.html">order tracking</a></li>
-                <li><a href="login-register.html">login / register </a></li>
-              </ul>
-            </li>
-            <li class="menu-item-has-children "><span class="menu-expand"><i></i></span><a href="#">Blog</a>
-              <ul class="dropdown" style="display: none;">
-                <li><a href="blog.html">blog standard </a></li>
-                <li><a href="blog-no-sidebar.html">blog no sidebar </a></li>
-                <li><a href="blog-right-sidebar.html">blog right sidebar</a></li>
-                <li><a href="blog-details.html">blog details</a></li>
-              </ul>
-            </li>
-            <li><a href="contact.html">Contact us</a></li>
-          </ul>
-        </nav>
-        <!-- mobile menu end -->
-      </div>
-      <div class="mobile-contact-info mobile-header-padding-border-4">
-        <ul>
-          <li><i class="icon-phone "></i> (+612) 2531 5600</li>
-          <li><i class="icon-envelope-open "></i> norda@domain.com</li>
-          <li><i class="icon-home"></i> PO Box 1622 Colins Street West Australia</li>
-        </ul>
-      </div>
-      <div class="mobile-social-icon">
-        <a class="facebook" href="#"><i class="icon-social-facebook"></i></a>
-        <a class="twitter" href="#"><i class="icon-social-twitter"></i></a>
-        <a class="pinterest" href="#"><i class="icon-social-pinterest"></i></a>
-        <a class="instagram" href="#"><i class="icon-social-instagram"></i></a>
-      </div>
-    </div>
-  </div>
-</div>
+</nav>
