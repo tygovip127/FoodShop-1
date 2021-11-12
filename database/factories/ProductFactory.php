741e2ad3613bfd92d04b83b9dddc069589f93616
Le Phu Quoc <tygovip119@gmail.com>
2021-11-12 10:45:01 +0700

    Nothing

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Product;
+use App\Models\Picture;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ProductFactory extends Factory
--- database/seeders/ProductTableSeeder.php
+++ database/seeders/ProductTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Picture;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 use PhpOffice\PhpSpreadsheet\Chart\Title;
@@ -15,6 +16,6 @@ class ProductTableSeeder extends Seeder
      */
     public function run()
     {
-          Product::factory(20)->create();
+        Product::factory(20)->create();
     }
 }
